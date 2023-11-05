9b69ecd8bf8df32989b1ed2ccfd1810472201fda
Faragas91 <bernhardlaus@gmx.at>
2023-11-05 14:02:46 +0100

    Beginn LPS

--- little_programms/Longest_Palandromic_Substring.py
+++ little_programms/Longest_Palandromic_Substring.py
@@ -1,16 +1,38 @@
 # Given a string s, return the longest palindromic substring in s.
 
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
+# Example 1:
+
+# Input: s = "babad"
+# Output: "bab"
+# Explanation: "aba" is also a valid answer.
+
+# Example 2:
+
+# Input: s = "cbbd"
+# Output: "bb"
+
+
+# class Solution:
+#     def longestPalindrome(self, s: str) -> str:
         
         
+# s = "babad"
+
+# # Eine Instanz der Solution-Klasse erstellen
+# solution = Solution()
+
+# # Die Methode aufrufen und das Ergebnis speichern
+# result = solution.longestPalindrome(s)
+
+# # Das Ergebnis anzeigen
+# print("Die Länge des Palindromes in s ist:", result)
+
 s = "babad"
+palindrom = ""
+max_palindrom = ""
 
-# Eine Instanz der Solution-Klasse erstellen
-solution = Solution()
+for i in range(len(s)):
+    if s[i] not in palindrom:
+        palindrom += s[i]
 
-# Die Methode aufrufen und das Ergebnis speichern
-result = solution.longestPalindrome(s)
-
-# Das Ergebnis anzeigen
-print("Die Länge des Palindromes in s ist:", result)
+print(palindrom)
