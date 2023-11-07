604d606cce61504bffd4d53060c3144c8e75bc14
Faragas91 <bernhardlaus@gmx.at>
2023-11-07 19:06:02 +0100

    Finish LPS, Beginn ZZC

--- little_programms/Longest_Palandromic_Substring.py
+++ little_programms/Longest_Palandromic_Substring.py
@@ -12,27 +12,40 @@
 # Output: "bb"
 
 
-# class Solution:
-#     def longestPalindrome(self, s: str) -> str:
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def expand_around_center(left, right, s):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
+
+        longest_palindrome = ""
         
-        
-# s = "babad"
+        for i in range(len(s)):
+            # Überprüfe, ob ein Palindrom mit dem aktuellen Zeichen in der Mitte beginnt.
+            palindrome1 = expand_around_center(i, i, s)
+            # Überprüfe, ob ein Palindrom mit den aktuellen und nächsten Zeichen in der Mitte beginnt.
+            palindrome2 = expand_around_center(i, i + 1, s)
+            
+            # Vergleiche die Länge der gefundenen Palindrome und aktualisiere das längste Palindrom.
+            if len(palindrome1) > len(longest_palindrome):
+                longest_palindrome = palindrome1
+            if len(palindrome2) > len(longest_palindrome):
+                longest_palindrome = palindrome2
 
-# # Eine Instanz der Solution-Klasse erstellen
-# solution = Solution()
+        return longest_palindrome
 
-# # Die Methode aufrufen und das Ergebnis speichern
-# result = solution.longestPalindrome(s)
+# Beispiel
+s = "aacabdkacaa"
 
-# # Das Ergebnis anzeigen
-# print("Die Länge des Palindromes in s ist:", result)
+# Eine Instanz der Solution-Klasse erstellen
+solution = Solution()
 
-s = "babad"
-palindrom = ""
-max_palindrom = ""
+# Die Methode aufrufen und das Ergebnis speichern
+result = solution.longestPalindrome(s)
 
-for i in range(len(s)):
-    if s[i] not in palindrom:
-        palindrom += s[i]
+# Das Ergebnis anzeigen
+print("Das längste Palindrom in s ist:", result)
 
-print(palindrom)
+
