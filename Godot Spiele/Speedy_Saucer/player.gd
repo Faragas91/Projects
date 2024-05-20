21528e722ee42941e32e014fabfbecb9f452e6a1
Stefan <bernhardlaus@gmx.at>
2024-05-20 11:34:58 +0200

    fusl

--- Godot Spiele/Speedy_Saucer/player.gd
+++ Godot Spiele/Speedy_Saucer/player.gd
@@ -1,7 +1,5 @@
 extends RigidBody2D
 
 func _ready():
-	return test()
-# Called when the node enters the scene tree for the first time.
-func test():
-	print("Wilde Sau!!!!")
+	apply_impulse(Vector2(95, 5))
+	
