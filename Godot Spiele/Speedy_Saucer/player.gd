78a2a77318f3a47a4b443d8f0383a55996f615f5
Stefan Redl <bernhardlaus@gmx.at>
2024-05-27 19:28:12 +0200

    gugl

--- Godot Spiele/Speedy_Saucer/player.gd
+++ Godot Spiele/Speedy_Saucer/player.gd
@@ -1,8 +1,7 @@
 extends RigidBody2D
 
-<<<<<<< HEAD
 var force = 1000
-	
+
 func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		apply_force(Vector2(force,0))
@@ -12,8 +11,3 @@ func _physics_process(delta):
 		apply_force(Vector2(0,-force))
 	if Input.is_action_pressed("move_down"):
 		apply_force(Vector2(0,force))
-=======
-func _ready():
-	apply_impulse(Vector2(95, 5))
->>>>>>> 21528e722ee42941e32e014fabfbecb9f452e6a1
-	
